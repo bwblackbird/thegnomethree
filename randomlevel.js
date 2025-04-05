10b4ac687b835dd970f43afa07779fe8e77fb319
cabbidge <121267327+cabbidge@users.noreply.github.com>
2025-04-05 16:15:33 -0500

    Upload simplex-noise library

--- randomlevel.js
+++ randomlevel.js
@@ -1,4 +1,4 @@
-import SimplexNoise from 'simplex-noise';
+import SimplexNoise from './lib/simplex-noise';
 
 export function generateRandomLevel(map, h, w){
     const noise = new SimplexNoise(); 
@@ -7,9 +7,9 @@ export function generateRandomLevel(map, h, w){
         for (let x = 0; x < w; x++) {
             let value = noise.noise2D(x / 5, y / 5); // scale of noise
             if (value > 0.15) {
-                setCell(x, y, 1); // Set wall
+                map.setCell(x, y, 1); // Set wall
             } else {
-                setCell(x, y, 0); // Set floor
+                map.setCell(x, y, 0); // Set floor
             }
         }
     }
